239e480caf807da4af3bf526b62875883ae0d919
Don Syme <donsyme@fastmail.fm>
2016-07-25 16:07:49 +0100

    more work

---
+++ concepts-sample2.fsx
@@ -0,0 +1,36 @@
+﻿
+[<Trait>]
+type MergeTrait<'T> =
+    abstract Merge : 'T * 'T -> 'T
+    abstract Empty : 'T 
+
+let trait<'T> = Unchecked.defaultof<'T>
+
+let mergeTwice<'T, 'U when 'U :> MergeTrait<'T>>(x : 'T ) =       
+    let x0 = trait<'U>.Empty   
+    let x2 = trait<'U>.Merge(x,x0)   
+    let x4 = trait<'U>.Merge(x2,x2)   
+    x4
+
+[<Witness>] 
+type MergeInt =  
+    interface MergeTrait<int> with
+        member __.Merge(a,b) = a + b
+        member __.Empty = 0
+
+type Box<'T>(x:'T) = 
+    member __.Value = x
+    override __.ToString() = sprintf "Box(%A)" x
+
+[<Witness>]
+type MergeBox<'T,'U when 'U :> MergeTrait<'T>> =
+    interface MergeTrait<Box<'T>> with
+        member __.Merge(a,b) = Box(trait<'U>.Merge(a.Value, b.Value))
+        member __.Empty = Box(trait<'U>.Empty)
+
+
+mergeTwice 4 |> printfn "%A"
+mergeTwice (Box(4)) |> printfn "%A"
+mergeTwice (Box(Box(4))) |> printfn "%A"
+mergeTwice (Box(Box(Box(4)))) |> printfn "%A"
+
