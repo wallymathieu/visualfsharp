b406e814757fcc12a2e66f28b40c1ebe420c410d
Rupert Horlick <t-ruhor@microsoft.com>
2016-07-27 16:55:49 +0100

    Finalised Beautiful Differentation implementation

--- examples/BeautifulDifferentation.fsx
+++ examples/BeautifulDifferentation.fsx
@@ -1,5 +1,4 @@
-﻿
-#I "bin"
+﻿#I "bin"
 #r "Numeric"
 
 open Numeric
@@ -9,8 +8,8 @@ open Numeric
 [<Witness>]
 type FloatingFunc<'a,'b,'c when 'c :> Floating<'b>> =
     interface Floating<'a -> 'b> with
-        member plus a b         = fun x -> Floating.plus (a x) (b x)
-        member times a b        = fun x -> Floating.times (a x) (b x)
+        member plus a b         = fun x -> a x + b x
+        member times a b        = fun x -> a x * b x
         member negate a         = Floating.negate << a
         member abs a            = Floating.abs << a
         member signum a         = Floating.signum << a
@@ -31,29 +30,22 @@ type FloatingFunc<'a,'b,'c when 'c :> Floating<'b>> =
         member acosh a          = Floating.acosh << a
         member atanh a          = Floating.atanh << a
 
+// Type of D
+
 type D<'a> = D of 'a * 'a
 
-let constD x = D(x, Floating.fromInteger 0)
+let constD x = D (x, Floating.fromInteger 0)
 
-let idD x = D(x, Floating.fromInteger 1)
+let idD x = D (x, Floating.fromInteger 1)
 
-let (|><|) f f' (D(a, a')) = D(f a, Floating.times a' (f' a))
+let (|><|) f f' (D(a, a')) = D (f a, a' * f' a)
 
-//FloatingD with |><| to generalise over scalar chain rule
+let sqr x = x * x
+
+// Ugly FloatingD that has repeated uses of chain rule
 
 (*[<Witness>]
-type FloatingD<'a, 'b, 'c when 'b :> Floating<'a> and 'c :> Floating<'a -> 'a>> =
-    interface Floating<D<'a>> with
-        member negate a = (Floating.negate |><| Floating.negate (Floating.fromInteger 1)) a*)
-
-
-// Witness for type D 'a
-
-let sqr (x : 'T when 'U :> Num<'T>) =
-    Num.times x x
-
-[<Witness>]
-type FloatingD<'a, 'b when 'b :> Floating<'a>> =
+type FloatingDUgly<'a, 'b when 'b :> Floating<'a>> =
     interface Floating<D<'a>> with
         member plus         (D(x,x')) (D(y,y')) = D (x + y, x' + y')
         member times        (D(x,x')) (D(y,y')) = D (x * y, y' * x + x' * y)
@@ -75,9 +67,36 @@ type FloatingD<'a, 'b when 'b :> Floating<'a>> =
         member cosh         (D(x,x'))           = D (Floating.cosh x, x' * Floating.sinh x)
         member asinh        (D(x,x'))           = D (Floating.asinh x, x' / (Floating.sqrt <| Floating.fromInteger 1 + sqr x))
         member acosh        (D(x,x'))           = D (Floating.acosh x, x' / -(Floating.sqrt <| sqr x - Floating.fromInteger 1))
-        member atanh        (D(x,x'))           = D (Floating.atanh x, x' / Floating.fromInteger 1 - sqr x)
+        member atanh        (D(x,x'))           = D (Floating.atanh x, x' / Floating.fromInteger 1 - sqr x)*)
 
-let plusTwice x =
+// FloatingD with |><| to generalise over scalar chain rule
+
+[<Witness>]
+type FloatingD<'a, 'b, 'c when 'b :> Floating<'a> and 'c :> Floating<'a -> 'a>> =
+    interface Floating<D<'a>> with
+        member plus         (D(x,x')) (D(y,y')) = D (x + y, x' + y')
+        member times        (D(x,x')) (D(y,y')) = D (x * y, y' * x + x' * y)
+        member negate       x                   = (Floating.negate  |><| Floating.negate (Floating.fromInteger 1)) x
+        member abs          x                   = (Floating.abs     |><| Floating.signum) x
+        member signum       x                   = (Floating.signum  |><| Floating.fromInteger 0) x
+        member fromInteger  x                   = constD <| Floating.fromInteger x
+        member recip        x                   = (Floating.recip   |><| -(sqr << Floating.recip)) x
+        member pi                               = constD Floating.pi
+        member exp          x                   = (Floating.exp     |><| Floating.exp) x
+        member log          x                   = (Floating.log     |><| Floating.recip) x
+        member sqrt         x                   = (Floating.sqrt    |><| (Floating.recip << Floating.fromInteger 2 * Floating.sqrt)) x
+        member sin          x                   = (Floating.sin     |><| Floating.cos) x
+        member cos          x                   = (Floating.cos     |><| -Floating.sin) x
+        member asin         x                   = (Floating.asin    |><| (Floating.recip << Floating.sqrt << Floating.fromInteger 1 - sqr)) x
+        member acos         x                   = (Floating.acos    |><| (Floating.recip << -Floating.sqrt << Floating.fromInteger 1 - sqr)) x
+        member atan         x                   = (Floating.atan    |><| (Floating.recip << Floating.fromInteger 1 + sqr)) x
+        member sinh         x                   = (Floating.sinh    |><| Floating.cosh) x
+        member cosh         x                   = (Floating.cosh    |><| Floating.sinh) x
+        member asinh        x                   = (Floating.asinh   |><| (Floating.recip << Floating.sqrt << Floating.fromInteger 1 + sqr)) x
+        member acosh        x                   = (Floating.acosh   |><| (Floating.recip << -Floating.sqrt << sqr - Floating.fromInteger 1)) x
+        member atanh        x                   = (Floating.atanh   |><| (Floating.recip << Floating.fromInteger 1 - sqr)) x
+
+let timesThree x =
     x + x + x
 
 let cube x =
@@ -92,10 +111,10 @@ let f2 x =
 let f3 x =
     Floating.sqrt <| Floating.fromInteger 3 * Floating.sin x
 
-plusTwice 2 |> printf "%d\n"
+timesThree 2 |> printf "%d\n"
 constD 1 |> printf "%A\n"
 idD 1 |> printf "%A\n"
-plusTwice (D(2.,1.)) |> printf "%A\n"
+timesThree (D(2.,1.)) |> printf "%A\n"
 cube (D(2.,1.)) |> printf "%A\n"
 f1 (D(2.,1.)) |> printf "%A\n"
 f2 (D(2.,1.)) |> printf "%A\n"
